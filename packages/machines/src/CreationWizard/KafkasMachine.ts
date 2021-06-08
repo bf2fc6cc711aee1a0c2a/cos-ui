74b8e472a93f70f418b11ca1fcf5e38ac9d62d42
Riccardo Forina <rforina@redhat.com>
2021-06-08 12:35:25 +0200

    Revert "Drop tsdx for building the packages"
    This reverts commit a1e7830948a545a50e4034984ddb767e531679e4.

--- packages/machines/src/CreationWizard/KafkasMachine.ts
+++ packages/machines/src/CreationWizard/KafkasMachine.ts
@@ -52,7 +52,7 @@ const fetchKafkaInstances = (
         : undefined;
     const search = [nameSearch, statusSearch].filter(Boolean).join(' AND ');
     apisService
-      .getKafkas(
+      .listKafkas(
         `${page}`,
         `${size}`,
         undefined,
