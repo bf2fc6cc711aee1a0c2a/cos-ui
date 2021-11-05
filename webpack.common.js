a9981297f4b1774d2192bca81a7bd62459a9be86
Riccardo Forina <rforina@redhat.com>
2021-11-05 11:22:48 +0100

    chore: make webpack compile with strict typescript checks (#249)

--- webpack.common.js
+++ webpack.common.js
@@ -29,7 +29,7 @@ module.exports = (env, argv) => {
             {
               loader: 'ts-loader',
               options: {
-                transpileOnly: true,
+                transpileOnly: false,
                 experimentalWatchApi: true,
               },
             },
