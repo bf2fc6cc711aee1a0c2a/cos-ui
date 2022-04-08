2aaf6d9f15072bce6a96d7dff16dcdf503539721
Indra <indraraj14@gmail.com>
2022-04-08 19:08:43 +0530

    MGDCTRS-757: fix: minor fixes (#444)
    * MGDCTRS-757: fix: minnor fixes
    
    * MGDCTRS-757: fix: format fix

--- src/app/components/RegisterEvalNamespace/RegisterEvalNamespace.tsx
+++ src/app/components/RegisterEvalNamespace/RegisterEvalNamespace.tsx
@@ -94,7 +94,12 @@ export const RegisterEvalNamespace: React.FC<RegisterEvalNamespaceProps> = ({
         >
           {t('register')}
         </Button>,
-        <Button key="cancel" variant="link" onClick={onModalToggle}>
+        <Button
+          key="cancel"
+          variant="link"
+          isDisabled={isLoading}
+          onClick={onModalToggle}
+        >
           {t('Cancel')}
         </Button>,
       ]}
