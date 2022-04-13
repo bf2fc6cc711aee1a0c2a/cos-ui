681546669738e03aa0aa2a307e55bc9ba7d3e056
Stan Lewis <gashcrumb@gmail.com>
2022-04-13 09:48:52 -0400

    chore: add more items per page options (#450)

--- src/app/components/Pagination/Pagination.tsx
+++ src/app/components/Pagination/Pagination.tsx
@@ -29,6 +29,14 @@ export const Pagination: FunctionComponent<PaginationProps> = ({
       title: '10',
       value: 10,
     },
+    {
+      title: '20',
+      value: 20,
+    },
+    {
+      title: '50',
+      value: 50,
+    },
   ];
   return (
     <PFPagination
