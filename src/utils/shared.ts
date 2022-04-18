2c4e0050c6f767a168a31324cba149f351e8992b
Stan Lewis <gashcrumb@gmail.com>
2022-04-18 12:11:29 -0400

    MGDCTRS758 fix: make credential fields editable
    This change changes the edit and duplicate page flows so that users can
    edit secret fields such as secret keys and client IDs that are part of
    JSON schema driven forms.  This commit also adds some text to unset
    fields to clearly indicate to the user that a configuration field is
    unset.  Also this change ensures that configuration that doesn't include
    a data shape is catered for client-side in both the duplicate and edit
    cases.  And finally, this commit also adds some help text to inform the
    user why a credential field's value isn't shown.

--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -9,6 +9,52 @@ export const mapToObject = (inputMap: Map<string, unknown>): object => {
 };
 
 /**
+ * Simple reference resolver that works with references contained in the
+ * same schema object
+ * @param ref
+ * @param schema
+ * @returns
+ */
+export const resolveReference = (ref: string, schema: Record<string, any>) => {
+  const [_, ...pathArray] = ref.split('/');
+  return pathArray.reduce((prev, key) => prev && prev[key], schema);
+};
+
+/**
+ * Get whatever the default value is for a given property in the given
+ * schema recursively
+ * @param propertyName
+ * @param schema
+ * @returns
+ */
+export const createDefaultFromSchema = (
+  propertyName: string,
+  schema: Record<string, any>
+) => {
+  const prop = schema.properties[propertyName];
+  if (typeof prop === 'undefined') {
+    return undefined;
+  }
+  const definition = prop.$ref
+    ? resolveReference(prop.$ref, schema)
+    : schema.properties[propertyName];
+  if (!definition.properties) {
+    return definition.default;
+  }
+  const answer: any = {};
+  Object.keys(definition.properties).map((key) => {
+    const value = createDefaultFromSchema(key, {
+      $defs: schema.$defs,
+      ...(definition || {}),
+    });
+    if (typeof value !== 'undefined') {
+      answer[key] = value;
+    }
+  });
+  return answer;
+};
+
+/**
  * Returns a new object that does not contain empty objects as values
  * and leaves the passed in value untouched.
  * @param obj
@@ -23,4 +69,25 @@ export const clearEmptyObjectValues = (obj: any): any => {
     }
   });
   return answer;
+};
+
+/**
+ * Applies client-side workarounds to possible discrepencies in the configuration.
+ *
+ * Applies a default datashape value from the schema if the configuration object doesn't have one.
+ *
+ * @param schema
+ * @param configuration
+ * @returns
+ */
+export const patchConfigurationObject = (
+  schema: Record<string, any>,
+  configuration: { data_shape: any; [key: string]: any }
+) => {
+  const { data_shape: dataShape, ...rest } = configuration;
+  if (typeof dataShape === 'undefined') {
+    const dataShape = createDefaultFromSchema('data_shape', schema);
+    return { ...rest, ...(dataShape && { data_shape: dataShape }) };
+  }
+  return configuration;
 };
