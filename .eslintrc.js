674e4fbac653cf8d49e6479705ef373469117e64
Stan Lewis <gashcrumb@gmail.com>
2022-05-25 15:11:47 -0400

    chore: update storybook to 6.5.5
    This change updates storybook and also adds the storybook eslint plugin
    as suggested by the update tool.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,19 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: [
-    'prettier', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-
-    'plugin:jest-dom/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:xstate/all',
-  ],
-  plugins: [
-    '@typescript-eslint',
-    'react-hooks',
-    'jest-dom',
-    'testing-library',
-    'xstate',
-  ],
+  extends: ['prettier', 'plugin:jest-dom/recommended', 'plugin:react-hooks/recommended', 'plugin:xstate/all', 'plugin:storybook/recommended'],
+  plugins: ['@typescript-eslint', 'react-hooks', 'jest-dom', 'testing-library', 'xstate'],
   settings: {
     react: {
-      version: '999.999.999',
-    },
+      version: '999.999.999'
+    }
   },
-  overrides: [
-    {
-      // 3) Now we enable eslint-plugin-testing-library rules or preset only for matching files!
-      files: [
-        '**/__tests__/**/*.[jt]s?(x)',
-        'test/?(*.)+(spec|test).[jt]s?(x)',
-      ],
-      extends: ['plugin:testing-library/react'],
-    },
-  ],
+  overrides: [{
+    // 3) Now we enable eslint-plugin-testing-library rules or preset only for matching files!
+    files: ['**/__tests__/**/*.[jt]s?(x)', 'test/?(*.)+(spec|test).[jt]s?(x)'],
+    extends: ['plugin:testing-library/react']
+  }],
   rules: {
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-  },
+    '@typescript-eslint/no-unused-vars': 'off'
+  }
 };
