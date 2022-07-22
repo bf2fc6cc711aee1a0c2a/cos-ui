6a6ab834da438dd64fbb7831545f36f90cabf829
Stan Lewis <gashcrumb@gmail.com>
2022-07-22 11:57:06 -0400

    Revert "MGDCTRS-1143 chore: update copy text and add link" (#533)

--- src/app/components/EmptyStateNoNamespace/EmptyStateNoNamespace.tsx
+++ src/app/components/EmptyStateNoNamespace/EmptyStateNoNamespace.tsx
@@ -4,17 +4,15 @@ import { useTranslation } from 'react-i18next';
 import {
   Bullseye,
   Button,
-  ButtonVariant,
   EmptyState,
   EmptyStateBody,
   EmptyStateIcon,
-  EmptyStateSecondaryActions,
   EmptyStateVariant,
   Title,
   TitleSizes,
   Tooltip,
 } from '@patternfly/react-core';
-import { ExternalLinkAltIcon, PlusCircleIcon } from '@patternfly/react-icons';
+import { PlusCircleIcon } from '@patternfly/react-icons';
 
 type EmptyStateNoNamespaceProps = {
   onModalToggle: () => void;
@@ -36,17 +34,6 @@ export const EmptyStateNoNamespace: FunctionComponent<EmptyStateNoNamespaceProps
               {t('createPreviewNamespace')}
             </Button>
           </Tooltip>
-          <EmptyStateSecondaryActions>
-            <Button
-              variant={ButtonVariant.link}
-              component={'a'}
-              target={'_blank'}
-              href={t('osdInstallationGuideLink')}
-              ouiaId={'empty-state-osd-guide-link'}
-            >
-              <ExternalLinkAltIcon /> {t('osdInstallationGuide')}
-            </Button>
-          </EmptyStateSecondaryActions>
         </EmptyState>
       </Bullseye>
     );
