e82252f8a6f92a8fa591655dd18c18c13eedabf3
Stan Lewis <gashcrumb@gmail.com>
2022-08-28 02:42:10 -0400

    chore: upgrade cypress (#566)
    This change migrates the integration tests to cypress 10.  Some
    important changes are cypress.config.json is now cypress.config.ts and
    the .spec.ts files are now .cy.ts files.

---
+++ cypress.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    env: {
+      homepage: '/',
+      wizard: '/create-connector',
+      connectorsApiPath:
+        '/localhost/api/connector_mgmt/v1/kafka_connectors?page=1&size=10&orderBy=&search=',
+      connectorsActionApiPath:
+        '/localhost/api/connector_mgmt/v1/kafka_connectors/',
+      connectorTypesApiPath:
+        '/localhost/api/connector_mgmt/v1/kafka_connector_types?page=1&size=20&search=',
+      kafkasApiPath:
+        '/localhost/api/kafkas_mgmt/v1/kafkas?page=1&size=10&search=',
+      namespaceApiPath:
+        '/localhost/api/connector_mgmt/v1/kafka_connector_namespaces/c9ns6husvba2r48dqnd0',
+      namespacesApiPath:
+        '/localhost/api/connector_mgmt/v1/kafka_connector_namespaces?page=1&size=10&search=',
+      serviceAccountApiPath: '/localhost/api/kafkas_mgmt/v1/service_accounts',
+      connectorCreationApiPath:
+        '/localhost/api/connector_mgmt/v1/kafka_connectors?async=true',
+    },
+    viewportWidth: 1280,
+    viewportHeight: 720,
+  },
+});
