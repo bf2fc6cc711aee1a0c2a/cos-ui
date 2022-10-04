33bfb6c70e97213ebdc45edc649016e0fbf1a8fe
Stan Lewis <gashcrumb@gmail.com>
2022-10-04 06:42:48 -0400

    MGDCTRS-1522 fix: tidy up the i18n config (#592)

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -6,7 +6,6 @@ import {
 } from 'date-fns';
 import { enUS } from 'date-fns/locale';
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
@@ -24,28 +23,22 @@ i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
   // learn more: https://github.com/i18next/i18next-http-backend
   .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    // Commenting it as we have fallbackLng for case of language detected as en-GB or en-US
-    // supportedLngs: ['en','it'],
     backend: {
       loadPath: `${__webpack_public_path__}locales/{{lng}}/{{ns}}.json`,
     },
     fallbackLng: 'en',
-    load: 'all',
+    load: 'languageOnly',
     debug: true,
     // add any namespaces you're using here for loading purposes
-    ns: ['public'],
-    defaultNS: 'public',
+    ns: ['cos-ui'],
+    defaultNS: 'cos-ui',
     nsSeparator: ':',
     keySeparator: '.',
-
     interpolation: {
       defaultVariables: undefined,
       escapeValue: false, // not needed for react as it escapes by default
