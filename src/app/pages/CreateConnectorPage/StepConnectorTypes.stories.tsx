32e3705605d4126bcd4a833a37e4946d0e7c5527
Stan Lewis <gashcrumb@gmail.com>
2022-10-06 15:54:42 -0400

    MGDCTRS-1477 chore: select connector type story
    This change adds a story that loads the Select Connector Type step from
    the wizard and mocks the API.  There's also a couple of small cleanup
    items, an update to the error string when the trying to use the wizard
    without it's provider and removal of an unused constant.

---
+++ src/app/pages/CreateConnectorPage/StepConnectorTypes.stories.tsx
@@ -0,0 +1,93 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { rest } from 'msw';
+import React from 'react';
+
+import { CreateConnectorWizardProvider } from '../../components/CreateConnectorWizard/CreateConnectorWizardContext';
+import { SelectConnectorType } from './StepConnectorTypes';
+
+const API_HOST = 'https://dummy.server';
+const API = `${API_HOST}/api/connector_mgmt/v1/kafka_connector_types`;
+
+export default {
+  title: 'Wizard Step 1/Connector Type Step',
+  component: SelectConnectorType,
+  decorators: [
+    (Story) => (
+      <CreateConnectorWizardProvider
+        accessToken={() => Promise.resolve('')}
+        connectorsApiBasePath={API_HOST}
+        kafkaManagementApiBasePath={''}
+        fetchConfigurator={() => Promise.resolve(undefined)}
+        onSave={(name: string) => console.debug('onSave, name: ', name)}
+      >
+        <Story />
+      </CreateConnectorWizardProvider>
+    ),
+  ],
+  args: {},
+  parameters: {
+    xstate: true,
+  },
+} as ComponentMeta<typeof SelectConnectorType>;
+
+const Template: ComponentStory<typeof SelectConnectorType> = () => (
+  <SelectConnectorType />
+);
+
+export const InitialLoading = Template.bind({});
+InitialLoading.parameters = {
+  msw: [
+    rest.get(API, (_req, res, ctx) => {
+      return res(ctx.delay(80000), ctx.status(403));
+    }),
+  ],
+};
+
+export const WithConnectorTypes = Template.bind({});
+WithConnectorTypes.parameters = {
+  msw: [
+    rest.get(`${API}*`, (req, res, ctx) => {
+      const searchParams = req.url.searchParams;
+      const page = +(searchParams.get('page') || 1);
+      const size = +(searchParams.get('size') || 20);
+      const search = searchParams.get('search') || '';
+      const response = generateStoryConnectorTypes(page, size, search, 60);
+      return res(ctx.delay(300), ctx.json(response));
+    }),
+  ],
+};
+
+export const WithError = Template.bind({});
+WithError.parameters = {
+  msw: [
+    rest.get(API, (_req, res, ctx) => {
+      return res(ctx.status(403));
+    }),
+  ],
+};
+
+// generate a deterministic set of connector types
+const generateStoryConnectorTypes = (page, size, search, total) => {
+  const fullSet = Array.from(
+    {
+      length: total,
+    },
+    (_, index) => ({
+      name: `Some Connector ${index}`,
+      id: `connector_${index}`,
+      version: `some_version_${index}`,
+      description: `This is a fancy connector description for Some Connector ${index}`,
+      labels: index % 2 ? ['source'] : ['sink'],
+    })
+  );
+  const start = (page - 1) * size;
+  const end = start + size;
+  return {
+    page,
+    size,
+    items: fullSet
+      .slice(start, end)
+      .filter(({ name }) => name.includes(search)),
+    total,
+  };
+};
