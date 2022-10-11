a81ad4370a11172ce49b13ffecbe1d6079a19da4
Indra <indraraj14@gmail.com>
2022-10-11 21:39:43 +0530

    MGDCTRS-882: fix:Show the boolean value on the review Page (#599)
    * MGDCTRS-882: fix:Show the boolean value on the review Page
    
    * MGDCTRS-882: fix:format fix

--- src/app/pages/CreateConnectorPage/StepReview.tsx
+++ src/app/pages/CreateConnectorPage/StepReview.tsx
@@ -2,7 +2,7 @@ import { UserProvidedServiceAccount } from '@apis/api';
 import { useReviewMachine } from '@app/components/CreateConnectorWizard/CreateConnectorWizardContext';
 import { StepBodyLayout } from '@app/components/StepBodyLayout/StepBodyLayout';
 import { ViewJSONFormat } from '@app/components/ViewJSONFormat/ViewJSONFormat';
-import { getPasswordType } from '@utils/shared';
+import { getPasswordType, maskValue } from '@utils/shared';
 import _ from 'lodash';
 import React, { useState, useCallback, FC } from 'react';
 
@@ -56,6 +56,27 @@ export function Review() {
     ></StepReviewComponent>
   );
 }
+
+const ConnectorProperties = (
+  property: string,
+  connectorProperties: { [key: string]: any },
+  dataToHide: string[]
+) => {
+  switch (typeof connectorProperties[property]) {
+    case 'object':
+      return property === 'data_shape' ? (
+        <DataShape data={connectorProperties[property]} />
+      ) : (
+        JSON.stringify(connectorProperties[property])
+      );
+    case 'boolean':
+      return connectorProperties[property].toString();
+    default:
+      return dataToHide.includes(property)
+        ? maskValue(connectorProperties[property])
+        : connectorProperties[property];
+  }
+};
 
 type StepReviewComponentProps = {
   kafka: KafkaRequest;
@@ -118,9 +139,6 @@ export const StepReviewComponent: React.FC<StepReviewComponentProps> = ({
     delete modifiedObject['error_handler'];
   } catch (e) {}
 
-  const maskValue = (value: any) => {
-    return '*'.repeat(value.length);
-  };
   return (
     <StepBodyLayout
       title={t('review')}
@@ -284,17 +302,7 @@ export const StepReviewComponent: React.FC<StepReviewComponentProps> = ({
                       <strong>{_.startCase(el)}</strong>
                     </GridItem>
                     <GridItem span={8}>
-                      {dataToHide.includes(el) ? (
-                        maskValue(modifiedObject[el])
-                      ) : typeof modifiedObject[el] === 'object' ? (
-                        el === 'data_shape' ? (
-                          <DataShape data={modifiedObject[el]} />
-                        ) : (
-                          JSON.stringify(modifiedObject[el])
-                        )
-                      ) : (
-                        modifiedObject[el]
-                      )}
+                      {ConnectorProperties(el, modifiedObject, dataToHide)}
                     </GridItem>
                   </Grid>
                 );
